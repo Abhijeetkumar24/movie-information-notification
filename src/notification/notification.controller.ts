2fea77c95be8d0e1f10ae8f08434808299410157
Abhijeet <abhijeet.kumar@appinventiv.com>
2023-11-24 10:40:04 +0530

    update

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,16 +1,14 @@
-import { Metadata, ServerUnaryCall } from '@grpc/grpc-js';
-import { Controller, RequestTimeoutException } from '@nestjs/common';
-import { EventPattern, GrpcMethod, MessagePattern } from '@nestjs/microservices';
-import { MESSAGE } from 'src/interface/enum';
-import { AddMovieRequest, AddMovieResponse } from 'src/interface/notification.interface';
+
+import { Controller} from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { MailerService } from '@nestjs-modules/mailer';
-import { Observable, from, of } from 'rxjs';
+import { NotificationService } from './notification.service';
 
 @Controller('notification')
 export class NotificationController {
 
     constructor(
-        private readonly mailerService: MailerService,
+        private readonly notificationService: NotificationService,
     ) { };
 
     // @GrpcMethod('NotificationService', 'AddMovie')
@@ -26,28 +24,14 @@ export class NotificationController {
 
 
     @MessagePattern('movie.add')
-    async movieAdd(payload: any) {
-        const mailOptions = {
-            to: payload.data.emails,
-            subject: MESSAGE.ADD_MOVIE_SUBJECT,
-            text: payload.title + MESSAGE.ADD_MOVIE_TEXT,
-        };
-
-        this.mailerService.sendMail(mailOptions);
-
-        return;
+    async movieAdd(payload: any): Promise<void> {
+        return this.notificationService.movieAdd(payload);
     }
-   
-    
+
+
     @MessagePattern('notification.add.comment')
-    async addComment(payload: any) {
-        const mailOptions = {
-            to: payload.subscriberEmails.emails,
-            subject: MESSAGE.ADD_COMMENT_SUBJECT,
-            text: `${payload.name.toUpperCase()}${MESSAGE.ADD_COMMENT_TEXT}"${payload.text}"${MESSAGE.ON_MOVIE}"${payload.movieName}"`,
-        };
-        this.mailerService.sendMail(mailOptions);
-
-        return MESSAGE.EMAIL_SEND;
+    async addComment(payload: any): Promise<string> {
+        return this.notificationService.addComment(payload);
     }
+
 }
