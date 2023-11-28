9977e5384a59a6c8a24a653d84880938b47fc735
Abhijeet <abhijeet.kumar@appinventiv.com>
2023-11-28 18:26:54 +0530

    Added JSDoc Comment

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -11,16 +11,37 @@ export class NotificationController {
     ) { };
 
   
-
+    /**
+     * Message pattern handler for the 'movie.add' event.
+     *
+     * @param {any} payload - The payload received with the 'movie.add' event.
+     * @returns {Promise<void>} A Promise that resolves when the movie addition notification process is completed.
+     * @throws {Error} Throws an error if there is an issue during the notification process.
+     */
     @MessagePattern('movie.add')
     async movieAdd(payload: any): Promise<void> {
-        return this.notificationService.movieAdd(payload);
+        try {
+            return this.notificationService.movieAdd(payload);
+        } catch (error) {
+            throw error;
+        }
     }
 
 
+
+    /**
+     * Message pattern handler for the 'notification.add.comment' event.
+     *
+     * @param {any} payload - The payload received with the 'notification.add.comment' event.
+     * @returns {Promise<string>} A Promise that resolves with a string when the comment addition notification process is completed.
+     * @throws {Error} Throws an error if there is an issue during the notification process.
+     */
     @MessagePattern('notification.add.comment')
     async addComment(payload: any): Promise<string> {
-        return this.notificationService.addComment(payload);
+        try {
+            return this.notificationService.addComment(payload);
+        } catch (error) {
+            throw error;
+        }
     }
-
 }
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -9,28 +9,50 @@ export class NotificationService {
         private readonly mailerService: MailerService,
     ) { }
 
+
+    /**
+     * Processes the addition of a movie and sends notification emails to subscribers.
+     *
+     * @param {any} payload - The payload containing movie and subscriber information.
+     * @returns {Promise<void>} A Promise that resolves when the movie addition notification emails are sent.
+     * @throws {Error} Throws an error if there is an issue during the notification process.
+     */
     async movieAdd(payload: any): Promise<void> {
-        const mailOptions = {
-            to: payload.data.emails,
-            subject: MESSAGE.ADD_MOVIE_SUBJECT,
-            text: payload.title + MESSAGE.ADD_MOVIE_TEXT,
-        };
+        try {
+            const mailOptions = {
+                to: payload.data.emails,
+                subject: MESSAGE.ADD_MOVIE_SUBJECT,
+                text: payload.title + MESSAGE.ADD_MOVIE_TEXT,
+            };
 
-        this.mailerService.sendMail(mailOptions);
-
-        return;
+            this.mailerService.sendMail(mailOptions);
+            return;
+        } catch (error) {
+            throw error;
+        }
     }
 
 
+
+    /**
+     * Processes the addition of a comment and sends notification emails to subscribers.
+     *
+     * @param {any} payload - The payload containing comment and subscriber information.
+     * @returns {Promise<string>} A Promise that resolves with a success message when the comment addition notification emails are sent.
+     * @throws {Error} Throws an error if there is an issue during the notification process.
+     */
     async addComment(payload: any): Promise<string> {
-        const mailOptions = {
-            to: payload.subscriberEmails.emails,
-            subject: MESSAGE.ADD_COMMENT_SUBJECT,
-            text: `${payload.name.toUpperCase()}${MESSAGE.ADD_COMMENT_TEXT}"${payload.text}"${MESSAGE.ON_MOVIE}"${payload.movieName}"`,
-        };
+        try {
+            const mailOptions = {
+                to: payload.subscriberEmails.emails,
+                subject: MESSAGE.ADD_COMMENT_SUBJECT,
+                text: `${payload.name.toUpperCase()}${MESSAGE.ADD_COMMENT_TEXT}"${payload.text}"${MESSAGE.ON_MOVIE}"${payload.movieName}"`,
+            };
 
-        this.mailerService.sendMail(mailOptions);
-
-        return MESSAGE.EMAIL_SEND;
+            this.mailerService.sendMail(mailOptions);
+            return MESSAGE.EMAIL_SEND;
+        } catch (error) {
+            throw error;
+        }
     }
 }
